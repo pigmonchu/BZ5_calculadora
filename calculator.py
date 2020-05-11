ed37691db8393d9ed173ffd86be60194b6718809
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-05-11 23:19:59 +0200

    Display layout completo

--- calculator.py
+++ calculator.py
@@ -4,15 +4,23 @@ from tkinter import ttk
 class Controlator(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, width=272, height=300)
-        
+        d = Display(self)
+        d.grid(column=0, row=0, columnspan=4)
+
+
+
 
 
 class Display(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, width=272, height=50)
-        self.pack_propagate(1)
+        self.pack_propagate(0)
 
-        lbl = ttk.Label(self, text="0", anchor=E)
+        s = ttk.Style()
+        s.theme_use('alt')
+        s.configure('my.TLabel', font='Helvetica 36', background='black', foreground='white')
+
+        lbl = ttk.Label(self, text="0", anchor=E, style='my.TLabel')
         lbl.pack(side=TOP, fill=BOTH, expand=True)
 
     

57fd331010713be35dcba1bf911416015e171408
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-05-12 21:26:50 +0200

    Command y lambdas en CalcButton

--- calculator.py
+++ calculator.py
@@ -6,110 +6,92 @@ dbuttons = [
         "text": "1",
         "col": 0,
         "row": 4,
-        "cmd": lambda: Controlator.pinta("1")
     },
     {
         "text": "2",
         "col": 1,
         "row": 4,
-        "cmd": lambda: Controlator.pinta("2")
     },
     {
         "text": "3",
         "col": 2,
         "row": 4,
-        "cmd": lambda: Controlator.pinta("3")
     },
     {
         "text": "+",
         "col": 3,
         "row": 4,
-        "cmd": lambda: Controlator.pinta("+")
     },
     {
         "text": "4",
         "col": 0,
         "row": 3,
-        "cmd": lambda: Controlator.pinta("4")
     },
     {
         "text": "5",
         "col": 1,
         "row": 3,
-        "cmd": lambda: Controlator.pinta("5")
     },
     {
         "text": "6",
         "col": 2,
         "row": 3,
-        "cmd": lambda: Controlator.pinta("6")
     },
     {
         "text": "-",
         "col": 3,
         "row": 3,
-        "cmd": lambda: Controlator.pinta("-")
     },
     {
         "text": "7",
         "col": 0,
         "row": 2,
-        "cmd": lambda: Controlator.pinta("7")
     },
     {
         "text": "8",
         "col": 1,
         "row": 2,
-        "cmd": lambda: Controlator.pinta("8")
     },
     {
         "text": "9",
         "col": 2,
         "row": 2,
-        "cmd": lambda: Controlator.pinta("9")
     },
     {
         "text": "x",
         "col": 3,
         "row": 2,
-        "cmd": lambda: Controlator.pinta("x")
     },
     {
         "text": "C",
         "col": 1,
         "row": 1,
-        "cmd": lambda: Controlator.pinta("C")
     },
     {
         "text": "+/-",
         "col": 2,
         "row": 1,
-        "cmd": lambda: Controlator.pinta("+/-")
     },
     {
         "text": "÷",
         "col": 3,
         "row": 1,
-        "cmd": lambda: Controlator.pinta("÷")
     },
     {
         "text": "0",
         "col": 0,
         "row": 5, 
         "W": 2,
-        "cmd": lambda: Controlator.pinta("0")
     },
     {
         "text": ",",
         "col": 2,
         "row": 5,
-        "cmd": lambda: Controlator.pinta(",")
     },
     {
         "text": "=",
         "col": 3,
         "row": 5,
-        "cmd": lambda: Controlator.pinta("=")
     }
 ]
 
@@ -127,10 +109,6 @@ class Controlator(ttk.Frame):
             btn = CalcButton(self, properties['text'], pinta, properties.get("W", 1), properties.get("H", 1))
             btn.grid(column=properties['col'], row=properties['row'], columnspan=properties.get("W", 1), rowspan=properties.get("H", 1))
 
-    @classmethod
-    def pinta(cls, valor):
-        print(valor)
-        return valor
 
 class Display(ttk.Frame):
     def __init__(self, parent):
